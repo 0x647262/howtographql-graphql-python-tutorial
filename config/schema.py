e58e50528f4ae3283510d850c45e5f9419aa960b
Dan R Beste <drb@wishalloy.com>
2021-07-26 14:31:27 -0500

    Feat: Complete part 2 of graphql-python

---
+++ config/schema.py
@@ -0,0 +1,9 @@
+import graphene
+
+import hackernews.links.schema
+
+class Query(hackernews.links.schema.Query, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query)
--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'graphene_django',
+    'hackernews.links',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
